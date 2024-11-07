7ffe8c6f4ce904509ae45de54197283408a6009a
muhammadharoon-01 <79999542+muhammadharoon-01@users.noreply.github.com>
2024-11-07 18:27:37 +0500

    EMS Console App.

---
+++ EMS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo
+            {
+                FileName = "J:\\SW-E2E\\01-ATM Repository\\Code Workspace\\Working\\EMSApplication.exe", // Replace with the application or script you want to run
+                UseShellExecute = true,
+                Verb = "runas" // This prompts the process to run as an administrator
+            };
+
+            // Start the process
+            Process process = Process.Start(processInfo);
+            process.WaitForExit(); // Optional: Waits for the process to complete
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("The process could not be started: " + ex.Message);
+        }
+    }
+}
